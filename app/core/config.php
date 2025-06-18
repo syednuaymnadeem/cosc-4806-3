aa172e2e5948d0fa54e5eae304c43df06cfa9467
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-06-18 02:14:31 +0000

    Updated the config file with the correct credentials and finally the database connection is established.

--- app/core/config.php
+++ app/core/config.php
@@ -14,8 +14,8 @@ define('LOGS', ROOT . DS . 'logs');
 define('FILES', ROOT . DS. 'files');
 
 // ---------------------  NEW DATABASE TABLE -------------------------
-define('DB_HOST',         'xxw.h.filess.io');
-define('DB_USER',         '4806_sentenceby'); 
+define('DB_HOST',         'cqeh8.h.filess.io');
+define('DB_USER',         'cosc4808_situation'); 
 define('DB_PASS',         $_ENV['DB_PASS']);
-define('DB_DATABASE',     '4806_sentenceby');
+define('DB_DATABASE',     'cosc4808_situation');
 define('DB_PORT',         '3305');
