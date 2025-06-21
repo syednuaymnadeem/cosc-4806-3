b80752d1baa7674811e1bbb9e20a02a6df8fc0d1
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-06-21 03:20:55 +0000

    I have edited the view function in the cintroller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,19 +4,26 @@ class Controller {
 
     public function __construct()
     {
-    
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
     }
-    
-    public function model ($model) {
-        require_once 'app/models/' .$model . '.php';
+
+    public function model($model)
+    {
+        require_once 'app/models/' . $model . '.php';
         return new $model();
     }
-    
-    public function view ($view, $data = []) {
-        require_once 'app/views/' . $view .'.php';
+
+    protected function view(string $path, array $data = []): void
+    {
+        $file = __DIR__ . '/../views/' . $path . '.php';
+        if (file_exists($file)) {
+            extract($data);
+            include $file;
+        } else {
+            echo "View not found: {$path}";
+        }
     }
 
 }
